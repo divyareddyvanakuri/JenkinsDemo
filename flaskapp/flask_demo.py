626140ef9d4f2ecd7e460ae98abcfd2b435119ce
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-05-09 10:47:22 +0530

    changed the text tag h2 to p

--- flaskapp/flask_demo.py
+++ flaskapp/flask_demo.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h2>Hello, World!</h2>"
+    return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
